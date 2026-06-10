1f28f0a5d572cca89f37e3b438fccfa7a2fe0979
Ishant Kulshreshtha <g25ait2041@iitj.ac.in>
2026-06-11 02:02:30 +0530

    committing changes for docker
    committing changes for docker

---
+++ src/inference.py
@@ -0,0 +1,23 @@
+import os
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+MODEL_NAME = os.getenv("HF_MODEL_NAME")
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
+
+def predict(text):
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
+
+    with torch.no_grad():
+        logits = model(**inputs).logits
+        return torch.argmax(logits, dim=1).item()
+
+
+if __name__ == "__main__":
+    TEXT = os.getenv("INPUT_TEXT", "This is a test sentence")
+    #print(predict(text))
+    print("Model:", MODEL_NAME)
+    print("Text:", TEXT)
+    print("Prediction ID:", predict(TEXT))
