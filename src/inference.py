1248e0e99f803a3952c697bc91de7b4bd1fd0103
Ishant Kulshreshtha <g25ait2041@iitj.ac.in>
2026-06-11 08:57:59 +0530

    update the inference

--- src/inference.py
+++ src/inference.py
@@ -1,11 +1,13 @@
 import os
+
 import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 MODEL_NAME = os.getenv("HF_MODEL_NAME")
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
+
 
 def predict(text):
     inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
@@ -17,7 +19,8 @@ def predict(text):
 
 if __name__ == "__main__":
     TEXT = os.getenv("INPUT_TEXT", "This is a test sentence")
-    #print(predict(text))
+
+    # Print prediction details
     print("Model:", MODEL_NAME)
     print("Text:", TEXT)
     print("Prediction ID:", predict(TEXT))
